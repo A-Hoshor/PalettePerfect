b1c7b548cec055774239aeee4ea7bcc4cff1fd97
A-Hoshor <alyssahoshor@gmail.com>
2024-03-21 19:56:18 -0400

    Resizing image added

--- test.py
+++ test.py
@@ -5,16 +5,19 @@ from sklearn.cluster import KMeans
 from PIL import Image
 from sklearn.metrics import pairwise_distances_argmin
 from sklearn.utils import resample
+import math
 
-n_colors = 8
+n_colors = 4
 
 image_path=("img/drone.JPG")
 image=Image.open(image_path)
+wimage = math.floor(image.width/2)
+himage = math.floor(image.height/2)
+image=image.resize((wimage, himage))
 
 imagearr= np.array(image)
 imagearr = imagearr.astype('float64')
 imagearr= imagearr/255
-
 
 print(imagearr)
 print(imagearr.shape)
@@ -24,6 +27,7 @@ h=imagearr.shape[1]
 d=imagearr.shape[2]
 assert d == 3 #returns assertion error if not 3
 print(imagearr.shape)
+
 
 #reshape array
 a = w*h
@@ -39,7 +43,7 @@ kmeans.fit(imagearr)
 labels=kmeans.predict(imagearr)
 
 book=resample(imagearr, random_state=0, n_samples=n_colors)
-labels_random = pairwise_distances_argmin(book, imagearr, axis=0)
+#labels_random = pairwise_distances_argmin(book, imagearr, axis=0)
 
 def recreate(book, labels, w, h):
     return book[labels].reshape(w,h,-1)
@@ -52,15 +56,18 @@ plt.axis("off")
 plt.title("Original image (96,615 colors)")
 plt.imshow(image)
 
+
 plt.figure(2)
 plt.clf()
 plt.axis("off")
 plt.title(f"Quantized image ({n_colors} colors, K-Means)")
 plt.imshow(recreate(kmeans.cluster_centers_, labels, w, h))
 
+"""
 plt.figure(3)
 plt.clf()
 plt.axis("off")
 plt.title(f"Quantized image ({n_colors} colors, Random)")
 plt.imshow(recreate(book, labels_random, w, h))
 plt.show()
+"""
